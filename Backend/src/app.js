d52b47966788c040928febceb1a29d11564f97ce
Tharunvasireddi <tharunvasireddy73@gmail.com>
2025-07-16 07:49:28 +0530

    chore: add vercel.json for backend deployment config

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -35,7 +35,7 @@ app.use("/api/get", userRouer);
 
 // error handler
 app.use(errorHandler);
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log("server is running url http://localhost:3000");
-});
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log("server is running url http://localhost:3000");
+// });
