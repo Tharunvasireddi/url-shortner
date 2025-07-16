cc4442c74ce1784f3a5a01698e1f62f926be30d0
Tharunvasireddi <tharunvasireddy73@gmail.com>
2025-07-16 22:16:10 +0530

    error is verified

--- Frontend/src/routing/home-route.js
+++ Frontend/src/routing/home-route.js
@@ -1,6 +1,6 @@
 import { createRoute } from "@tanstack/react-router";
 import { rootRoute } from "./routeTree";
-import Homepage from "../pages/Homepage.js";
+import Homepage from "../pages/Homepage";
 
 const homeRoute = createRoute({
   getParentRoute: () => rootRoute,
