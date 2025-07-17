72a92cf4dae3603411702f0286e0a7697fcc10a3
Tharunvasireddi <tharunvasireddy73@gmail.com>
2025-07-17 17:13:34 +0530

    cors configuration and axios configuration is set

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,10 +23,7 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
