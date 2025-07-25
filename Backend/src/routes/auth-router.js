114e067913bf24142166ef1b83dfdbab43c021ef
srinivaskoruprolu5 <srinivaskoruprolu5@gmail.com>
2025-07-25 04:34:59 +0000

    auth : added logout controller and logout route for logout functionality

--- Backend/src/routes/auth-router.js
+++ Backend/src/routes/auth-router.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   get_current_user,
   loginController,
+  logoutController,
   registerController,
 } from "../controllers/auth-controller.js";
 import { authMiddleware } from "../middlewares/auth-middleware.js";
@@ -11,4 +12,5 @@ const authRouter = express.Router();
 authRouter.post("/register", registerController);
 authRouter.post("/login", loginController);
 authRouter.get("/me", authMiddleware, get_current_user);
+authRouter.get("/logout", logoutController)
 export { authRouter };
